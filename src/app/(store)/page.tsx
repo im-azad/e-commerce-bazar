88e43fd3696e04487690e51003cfb1ff3a2b82f1
Abul Kalam Azad <me.azad99@gmail.com>
2025-02-01 17:52:36 +0600

    shadcn added

--- src/app/(store)/page.tsx
+++ src/app/(store)/page.tsx
@@ -1,6 +1,12 @@
+import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
-    <h1>Hello</h1>
+    <div className="flex flex-col items-center justify-center min-h-screen">
+      
+      <h1>Store</h1>
+      <Button>Click</Button>
+      
+      </div>
   );
 }
